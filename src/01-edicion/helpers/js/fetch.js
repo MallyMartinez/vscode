cd5cd524ed0954482e823571fe3625bc770b95d4
MallyMartinez <mally.hernandez2861@alumnos.udg.mx>
2024-08-11 17:23:36 -0600

    Crear nuevo archivo rapido

---
+++ src/01-edicion/helpers/js/fetch.js
@@ -0,0 +1,10 @@
+// *    para hacer un archivo mas rapido con 
+// *    rutas se hace lo siguiente
+// *    se crea un archivo y se pone el nombre con la ruta que
+// *    
+// ?    Crear nuevo archivo ->helpers/js/fetch.js
+// *
+// *    automaticamente se crea junto a las carpetas 
+// *    correspondiente de la ruta
+// *
+// ?    Crear nuevo folder -> helpers/js -> solo para crear carpetas (sin archivo)
